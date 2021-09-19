ab939e98a9b99de9896bb1b386cb8003d1f32cc1
Angello Nunez <angellonzgz@gmail.com>
2021-09-19 18:21:14 -0500

    Gabriel case

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("dateInput").addEventListener("change", function() {
+    var input = this.value;
+    console.log(input);
+
+    if (input == "1997-11-12") {
+        window.open("gabriel.html");
+    }})
