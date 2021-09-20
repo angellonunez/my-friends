e9f78da2ce8b48552b51184b84873e3697de56cc
Angello Nunez <angellonzgz@gmail.com>
2021-09-20 16:49:07 -0500

    First version

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 document.getElementById("dateInput").addEventListener("change", function() {
     var input = this.value;
-    console.log(input);
 
-    if (input == "1997-11-12") {
+    if (input == "1997-09-27") {
+        window.open("angello.html", "_self");
+    }   else if (input == "1997-11-12") {
         window.open("gabriel.html", "_self");
-    }})
+    }   else if (input == "1997-06-10") {
+        window.open("gustavo.html", "_self");
+    }   else if (input == "1997-09-01") {
+        window.open("alonso.html", "_self");
+    }   else {
+        window.open("nobody.html", "_self");
+    }
+})
