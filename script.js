342eaae220d33d2b8fd04ecfc5c7364377b2ef1e
Angello Nunez <angellonzgz@gmail.com>
2021-09-20 16:53:37 -0500

    Gustavo's birthday fixed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("dateInput").addEventListener("change", function() {
         window.open("angello.html", "_self");
     }   else if (input == "1997-11-12") {
         window.open("gabriel.html", "_self");
-    }   else if (input == "1997-06-10") {
+    }   else if (input == "1997-06-27") {
         window.open("gustavo.html", "_self");
     }   else if (input == "1997-09-01") {
         window.open("alonso.html", "_self");
